9c984df9013fea1ca4fe3521041174c0c9ea8bb9
briana escobar <106976343+brianaae@users.noreply.github.com>
2022-08-04 17:35:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+import './reset.css'
+import './style.css'
+
+const menuToggle = document.querySelector('.menu-toggle')
+const menu = document.querySelector('.menu')
+
+const initMenu =() => {
+  if( window.innerWidth < 768) {
+    menu.classList.add('hidden')
+  } else {
+    menu.classList.remove('hidden')
+  }
+
+}
+
+menuToggle.addEventListener('click', () => menu.classList.toggle('hidden'))
+window.addEventListener('resize', initMenu)
+initMenu()
